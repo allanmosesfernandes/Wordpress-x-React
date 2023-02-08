7dc02d8d9fad14b643b226133e8a5566837239fd
Allan Fernandes <Fernzallan14@gmail.com>
2023-02-08 17:35:11 +0000

    blog-preview-css

--- src/components/BlogPreview/BlogPreview.jsx
+++ src/components/BlogPreview/BlogPreview.jsx
@@ -2,20 +2,18 @@ import { React, useState, useEffect } from "react";
 import axios from "axios";
 import "./blogPreview.scss";
 
-
-
 const BlogPreview = () => {
     const [posts, setPosts] = useState([]);
 
-  useEffect(() => {
-    axios.get(`http://allanmoses.ninja/wp-json/wp/v2/unsolicited?_embed`)
-      .then(response => {
-        setPosts(response.data);
-      })
-      .catch(error => {
-        console.error(error);
-      });
-  }, []);
+    useEffect(() => {
+        axios.get(`http://allanmoses.ninja/wp-json/wp/v2/unsolicited?_embed`)
+        .then(response => {
+            setPosts(response.data);
+        })
+        .catch(error => {
+            console.error(error);
+        });
+    }, []);
 
 
   return (
@@ -24,11 +22,11 @@ const BlogPreview = () => {
         const imageURL = post["_embedded"]["wp:featuredmedia"][0]["source_url"];
         //==Get the date from API and extract what you need==//
         const dateString = post.date.split("T")[0];
-        console.log(dateString);
+        
         let blogDate = new Date(`${dateString}`).toDateString();
-        // const year = dateString.split("-")[0];
-         return (
-            <div className="blog__preview__card" key={index}>
+        
+        return (
+            <div className="blog__preview__card" key={index} title={post.title.rendered}>
                 <div className="card__number">0{index + 1}</div>
                 <span className="card__date">{blogDate}</span>
                 <div className="card__image">
