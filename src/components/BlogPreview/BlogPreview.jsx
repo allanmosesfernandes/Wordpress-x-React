6feff9d3aa73d27f82dfda646847aa5693191c26
Allan Fernandes <Fernzallan14@gmail.com>
2023-02-08 15:29:23 +0000

    image and blog card

--- src/components/BlogPreview/BlogPreview.jsx
+++ src/components/BlogPreview/BlogPreview.jsx
@@ -1,20 +1,37 @@
-import React from "react";
+import { React, useState, useEffect } from "react";
 import axios from "axios";
 import "./blogPreview.scss";
 
-axios.get(`http://allanmoses.ninja/wp-json/wp/v2/unsolicited`)
-  .then(response => {
-    console.log(response.data);
-  })
-  .catch(error => {
-    console.error(error);
-  }); 
 
-  
+
 const BlogPreview = () => {
+    const [posts, setPosts] = useState([]);
+
+  useEffect(() => {
+    axios.get(`http://allanmoses.ninja/wp-json/wp/v2/unsolicited?_embed`)
+      .then(response => {
+        setPosts(response.data);
+      })
+      .catch(error => {
+        console.error(error);
+      });
+  }, []);
+
+
   return (
-    <div>
-        <h2>Blog</h2>
+    <div className="blog__preview__container">
+    {posts.map((post,index) => {
+         const imageURL = post["_embedded"]["wp:featuredmedia"][0]["source_url"];
+         console.log(imageURL);
+         return (
+            <div className="blog__preview__card">
+                <div className="card__number">0{index + 1}</div>
+                <div className="card__image">
+                    <img src={imageURL} alt={`Image of post ${index + 1}`} />
+                </div>
+            </div>
+         )
+      })}
     </div>
   )
 }
